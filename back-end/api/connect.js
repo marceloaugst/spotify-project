004c9dbe447fdf572091881b84b96e4f1c73dfdd
MARCELO AUGUSTO ALVES FARIAS <marcelo.augsd@gmail.com>
2025-02-19 20:31:00 -0400

    ajudes dee process 2

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,7 +4,7 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://marcelo:admin123@cluster0.5fmzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
